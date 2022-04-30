39230d5f48c09631b2e133d9ec7c497aed874b2e
Valentina <valentina.francoa@upb.edu.co>
2022-04-29 19:02:50 -0500

    pruebas unitarias

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlJugador : MonoBehaviour
 {
     [SerializeField] private Jugador jugador;
+    [SerializeField] private Text poderUI;
 
     private float velocidad = 5f;
     private Vector3 posicionDestino;
     // Start is called before the first frame update
     void Start()
     {
+        poderUI.text = jugador.poder.ToString();
         posicionDestino = this.transform.position;
     }
 
