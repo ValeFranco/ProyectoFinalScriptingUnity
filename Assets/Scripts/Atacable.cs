f3a60e837a958a8656895dcc8b359fff8c1f7680
Sofía Díaz Z <67989042+SofiaDiazZ@users.noreply.github.com>
2022-04-30 19:27:39 -0500

    Script atacable

--- Assets/Scripts/Atacable.cs
+++ Assets/Scripts/Atacable.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Atacable : MonoBehaviour
 {
+    internal uint poder;
+    internal bool esObstaculo;
+
+    public Atacable(bool esObstaculo, uint poder)
+    {
+        this.esObstaculo = esObstaculo;
+        Poder = poder;
+    }
+
+    public uint Poder
+    {
+        get => poder; set
+
+        {
+            if (value > 0)
+                poder = value;
+            else
+                throw new Exception("el poder debe ser mayor a cero");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
