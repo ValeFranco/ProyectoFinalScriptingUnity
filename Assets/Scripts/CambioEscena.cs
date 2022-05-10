eb38eaab1e95fe8b6d40451e0193f2fb0fb871a1
Sofía Díaz Z <67989042+SofiaDiazZ@users.noreply.github.com>
2022-05-10 11:21:42 -0500

    pantalla de inicio

---
+++ Assets/Scripts/CambioEscena.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour
+{
+    public int numeroEscena;
+    public void Iniciar()
+    {
+        SceneManager.LoadScene(numeroEscena);
+    }
+}
