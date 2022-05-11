79a00be2898a3df5d2ddff660b15664ae3a5e10c
Valentina <valentina.francoa@upb.edu.co>
2022-05-10 23:08:29 -0500

    factory funcional

--- Assets/Scripts/Factory -Pick ups/PickUpSpawner.cs
+++ Assets/Scripts/Factory -Pick ups/PickUpSpawner.cs
@@ -8,30 +8,35 @@ public class PickUpSpawner : MonoBehaviour
 {
     [SerializeField] FactoryFacade factory;
     private GameObject lastPickUp;
-   // int numero = Random.Range(0, 2);
-    
+    int numero;
+    private void Start()
+    {
+         numero = Random.Range(0, 2);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
-            GetPickUp(PickUpType.buff);
-        else if (Input.GetKeyDown(KeyCode.D))
         {
-            GetPickUp(PickUpType.deBuff);
+            if(numero == 0)
+            {
+                GetPickUp(PickUpType.buff);
+            }
+            else
+            {
+                GetPickUp(PickUpType.deBuff);
+            }
         }
 
     }
-    private void Start()
-    {
-        
-    }
-
+   
     private void GetPickUp(PickUpType pickUpType)
     {
         if (lastPickUp != null)
             Destroy(lastPickUp);
 
         lastPickUp = factory?.GetNewPickUp(pickUpType);
-        Vector3 posicionDestino = new Vector3(-5, 2, 0);
+        Vector3 posicionDestino = new Vector3(-5.5f, 0.57f, 0);
         lastPickUp.transform.position = posicionDestino;
     }
 
